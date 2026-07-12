88a4b3988c8dbecce166052090d2abade44594d5
itspratishthasood <pratishthasood34@gmail.com>
2026-07-12 15:27:03 +0530

    Initial commit

---
+++ src/data/testimonial-data.js
@@ -0,0 +1,60 @@
+import user_1 from '@assets/img/users/user-1.jpg';
+import user_2 from '@assets/img/users/user-2.jpg';
+import user_3 from '@assets/img/users/user-3.jpg';
+import user_4 from '@assets/img/users/user-4.jpg';
+
+// fashion_testi_data
+export const fashion_testi_data = [
+  {
+    id:1,
+    review:4,
+    desc:'“Amazing product and great service!”',
+    user:user_1,
+    name:'lumea customer',
+    designation:'beauty lover'
+  },
+  {
+    id:2,
+    review:5,
+    desc:'“Very happy with our choice to take our daughter to Brave care. The entire team was great! Thank you!”',
+    user:user_3,
+    name:'John Smith',
+    designation:'UI/UX Designer'
+  },
+  {
+    id:3,
+    review:3,
+    desc:'“Thanks for all your efforts and teamwork over the last several months!  Thank you so much”',
+    user:user_4,
+    name:'Salim Rana',
+    designation:'Web Developer'
+  },
+]
+
+// beauty testimonial data 
+export const beauty_testi_data = [
+  {
+    id:1,
+    review:4,
+    desc:'Amazing product and great service!.',
+    user:user_1,
+    name:'kartik',
+    designation:'CO Founder'
+  },
+  {
+    id:2,
+    review:4.5,
+    desc:'The textures are luxurious and the results are stunning. My skin feels hydrated and glowing every day.',
+    user:user_2,
+    name:'daksh',
+    designation:'customer'
+  },
+  {
+    id:3,
+    review:5,
+    desc:' I love the natural finish and long-lasting effect of these products. They feel premium and gentle on the skin',
+    user:user_3,
+    name:'ishika',
+    designation:'designer'
+  },
+]
